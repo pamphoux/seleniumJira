f443416f7ef9cb10d9e7f99e7114c0cebe4b5eac
pamphoux <108572694+pamphoux@users.noreply.github.com>
2022-07-12 19:54:44 +0200

    Delete JiraCreateIssue.java

--- JiraCreateIssue.java
+++
@@ -1,9 +0,0 @@
-package com.demo.utility;
-
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Retention;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface JiraCreateIssue {
- boolean isCreateIssue();
-}
