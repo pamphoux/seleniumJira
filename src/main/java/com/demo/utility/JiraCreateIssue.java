0e7735ccbf7a31d2f5b6041b938d5392be488308
pamphoux <108572694+pamphoux@users.noreply.github.com>
2022-07-13 21:25:42 +0200

    Create JiraCreateIssue.java

---
+++ src/main/java/com/demo/utility/JiraCreateIssue.java
@@ -0,0 +1,11 @@
+//JiraCreateIssue.java
+
+package com.demo.utility;
+
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JiraCreateIssue {
+ boolean isCreateIssue();
+}
