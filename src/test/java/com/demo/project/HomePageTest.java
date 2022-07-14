755e9a2aeb89deaf85366f6a38aeea6d7e044d57
pamphoux <108572694+pamphoux@users.noreply.github.com>
2022-07-14 06:25:56 +0200

    Update HomePageTest.java

--- src/test/java/com/demo/project/HomePageTest.java
+++ src/test/java/com/demo/project/HomePageTest.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import src.main.java.com.demo.utility.JiraCreateIssue;
+import com.demo.utility.JiraCreateIssue;
 
 public class HomePageTest {
 	private WebDriver driver;
